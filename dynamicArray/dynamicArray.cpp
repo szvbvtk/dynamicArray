a9be4a5723bae7c60a06301f18ca81ef3b7071cc
szvbvt <krystian.szabat@outlook.com>
2022-10-17 19:11:37 +0200

    koniec

--- dynamicArray/dynamicArray.cpp
+++ dynamicArray/dynamicArray.cpp
@@ -59,9 +59,15 @@ struct DynamicArray {
         array = new T[capacity];
     }
 
-    string str() {
+    string str(string (*func)(T so)) {
 
         string s = "Actual size : " + to_string(size) + "\nCapacity : " + to_string(capacity) + "\n";
+
+        for (int i = 0; i < size; i++) {
+            s += func(array[i]);
+        }
+
+        s += '\n';
 
         return s;
 
@@ -90,6 +96,11 @@ struct simple_object {
 
 };
 
+string so_str(simple_object so) {
+    return "First name: " + so.first_name + "; second name: " + so.second_name + "; age: " + to_string(so.age) + "\n";
+}
+
+
 int simple_object_comparator(simple_object * so1, simple_object * so2) {
     if (so1->age == so2->age)
         return 0;
@@ -107,41 +118,46 @@ ostream& operator<<(ostream& os, simple_object so) {
 int main()
 {
     setlocale(LC_ALL, "");
+    srand(time(NULL));
+    //char alphabet[] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','x','z' };
+
     DynamicArray<simple_object>* array1 = new DynamicArray<simple_object>();
-    //cout << array1->str();
-    //array1->add(5);
-    //cout << array1->str();
-    //array1->add(6);
-    //cout << array1->str();
-    //array1->add(7);
-    //cout << array1->str();
-    //array1->add(9);
-    //cout << array1->str();
-    //array1->add(10);
-    //cout << array1->str();
+    //array1->add({ "Krystian", "Szabat", 21 });
+    //array1->change(0, { "Patrick", "Gendeck", 22 });
+    //array1->add({ "Paweł", "Rzepecki", 20 });
+    //array1->add({ "Maciej", "Jankowski", 16 });
+    //cout << array1->str(so_str);
+    //cout << array1->get(0);
+    //cout << array1->get(1);
+    //cout << array1->get(2);
 
-    //cout << array1->get(0) << endl;
-    //cout << array1->get(4);
+    //cout << '\n';
+    //array1->sort(simple_object_comparator);
+    //cout << array1->str(so_str);
 
-    //array1->clear();
-    //cout << array1->str();
+    clock_t start = clock();
 
-    array1->add({ "Krystian", "Szabat", 21 });
-    array1->add({ "Patrick", "Gendeck", 22 });
-    array1->add({ "Paweł", "Rzepecki", 20 });
-    array1->add({ "Maciej", "Jankowski", 16 });
-    cout << array1->str();
-    cout << array1->get(0);
-    cout << array1->get(1);
-    cout << array1->get(2);
-    cout << array1->get(3);
+    double max_measured_time_per_element = 0.0;
+    for (int i = 0; i < 10000000; i++) {
+        simple_object obj{"Marcin", "Lew", rand() % 30};
 
-    cout << '\n';
-    array1->sort(simple_object_comparator);
-    cout << array1->get(0);
-    cout << array1->get(1);
-    cout << array1->get(2);
-    cout << array1->get(3);
+        clock_t start_time = clock();
+        array1->add(obj);
+        clock_t end_time = clock();
+
+        double measured_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+        if (measured_time > max_measured_time_per_element) {
+            max_measured_time_per_element = measured_time;
+            cout << "New worst time: " << measured_time << " index: " << i << ' ';
+            measured_time = 0;
+        }
+
+    }
+
+    clock_t stop = clock();
+
+    cout << "\n\n\nExecution time: " << (double)(stop - start) / CLOCKS_PER_SEC << "\n\n\n";
 
     delete array1;
 }
